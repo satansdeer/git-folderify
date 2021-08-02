c56932f5dd6bfe2c501120dde04d11773f24ac81
Maksim Ivanov <satansdeer@gmail.com>
2021-08-02 16:30:57 +0200

    02-checkout-to-commits

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ import { promisify } from "util"
 import { exec } from "child_process"
 const execAsync = promisify(exec)
 
-const { stdout } = await execAsync("git log")
-console.log(stdout)
+const { stdout, stderr } = await execAsync("git log --oneline")
+if(!stdout){
+	throw(stderr)
+}
+const commits = stdout.split("\n").filter(line => line !== "").map(line => {
+	const [hash, ...nameArray] = line.split(" ")
+	return {hash, name: nameArray.join(" ")}
+})
+
+for (const commit of commits) {
+	await execAsync(`git checkout ${commit.hash}`)
+	console.log(await execAsync("git status"))
+	console.log('---', commit.name)
+	await execAsync(`git checkout main`)
+}
+
+console.log(commits)
