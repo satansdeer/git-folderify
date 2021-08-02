941b586752b8e7bf47dd29afe40b1ceab071dfa6
Maksim Ivanov <satansdeer@gmail.com>
2021-08-02 16:19:30 +0200

    01-git-log

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ import { promisify } from "util"
 import { exec } from "child_process"
 const execAsync = promisify(exec)
 
-const { stdout } = await execAsync("git --version")
+const { stdout } = await execAsync("git log")
+console.log(stdout)
